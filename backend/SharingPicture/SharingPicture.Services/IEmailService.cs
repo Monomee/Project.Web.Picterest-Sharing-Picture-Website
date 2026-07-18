120711ab32d157ca3ff54bb82e57cd9d6a6a69f5
MONOME\hoang <hoangthth12@gmail.com>
2026-07-18 18:24:50 +0700

    fix some stuff

--- backend/SharingPicture/SharingPicture.Services/IEmailService.cs
+++ backend/SharingPicture/SharingPicture.Services/IEmailService.cs
@@ -5,4 +5,5 @@ namespace SharingPicture.Services;
 public interface IEmailService
 {
     Task SendEmailAsync(string toEmail, string subject, string htmlMessage);
+    Task SendResetPasswordEmailAsync(string toEmail, string token);
 }
